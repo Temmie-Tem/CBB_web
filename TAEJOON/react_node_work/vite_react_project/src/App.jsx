9ba61528350f258daddae7ce398f3b917b1f5bc6
TAEJOON <hazukinoharuka@gmail.com>
2025-06-20 18:09:40 +0900

    day_1_class_fin

--- TAEJOON/react_node_work/vite_react_project/src/App.jsx
+++ TAEJOON/react_node_work/vite_react_project/src/App.jsx
@@ -8,17 +8,23 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='search_box'>
+        <button className='search_filter_button'>
+          <span className='search_filter_text'>
+            카테고리
+          </span>
+          <span className='search_filter_icon'>
+            V
+          </span>
+        </button>
+
+        <button className="search_button">
+          검색
+        </button>
       </div>
 
       <h1>Vite Only</h1>
-
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -27,7 +33,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
