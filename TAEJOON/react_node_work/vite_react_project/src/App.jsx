30a51d98e984d85ed274871c4acd8f2aca25132f
TAEJOON <hazukinoharuka@gmail.com>
2025-06-22 01:32:06 +0900

    day_3_4

--- TAEJOON/react_node_work/vite_react_project/src/App.jsx
+++ TAEJOON/react_node_work/vite_react_project/src/App.jsx
@@ -67,21 +67,6 @@ function App() {
       <BoardList />
       {/* 개시글 목록 */}
 
-      <h1>Vite Only</h1>
-      
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
     </div>
   )
 }
