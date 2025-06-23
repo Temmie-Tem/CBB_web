b42e0a5ecd0ef4891d178c378ac8519d889ae9ad
Temmie-tem <shs02140@gmail.com>
2025-06-23 18:06:14 +0900

    게시판 글 작성 페이지 병합

--- Gonna_be_OK/src/App.jsx
+++ Gonna_be_OK/src/App.jsx
@@ -5,6 +5,7 @@ import TermsPage from './components/TermsPage';   // 이용약관 컴포넌트
 import PrivacyPolicyPage from './components/PrivacyPolicyPage'; // 개인정보처리방침 컴포넌트
 import MainPage from './components/MainPage'; //홈페이지
 import LoginPage from './components/loginpage'; //로그인 페이지
+import PostWritePage from './components/PostWritePage'; // 게시판
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/privacy" element={<PrivacyPolicyPage />} />
         <Route path="/main" element={<MainPage/>}/>
         <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/PostWritePage' element={<PostWritePage/>}/>
         {/* 앱의 기본 페이지 설정 */}
         <Route path="/" element={<MainPage />} /> 
       </Routes>
