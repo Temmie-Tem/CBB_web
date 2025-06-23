b2a1ed35572d60207d69a633c23229ad307555ec
TAEJOON <hazukinoharuka@gmail.com>
2025-06-23 09:42:15 +0900

    해더 작성중

--- TAEJOON/react_node_work/vite_react_project/src/components/Header.jsx
+++ TAEJOON/react_node_work/vite_react_project/src/components/Header.jsx
@@ -3,8 +3,16 @@ import '../CSS/Header_style.css';
 
 function Header() {
     return (
-        <header>
-            
+        <header className='main_header'>
+            <div className='header_wrap'>
+                <div className='header_banner'>
+                    <img src="" alt="image" />
+                    <span>TEXT</span>
+                </div>
+                <nav className='header_nav'>
+
+                </nav>
+            </div>
         </header>
     )
 
