8775b603ef833e56f523526a3b78a58bd7be0aad
TAEJOON <hazukinoharuka@gmail.com>
2025-06-23 14:57:25 +0900

    이름/경로 변경 완료.

--- TAEJOON/react_node_work/vite_react_project/src/components/Footer.jsx
+++ TAEJOON/react_node_work/vite_react_project/src/components/Footer.jsx
@@ -9,34 +9,34 @@ function Footer() {
                 {/* table식과 유사하게 4개의 div로 의사적인 표의 형태를 생성. */}
                 <div className="footer_column">
                     <h3>상담게시판</h3> 
-                    <a href="">사람</a> <a href="">동물</a> <a href="">식물</a>
+                    <a>사람</a> <a>동물</a> <a>식물</a>
                     {/* 개별적인 요소로 기능하도록 <a>를 활용. 향후 링크삽입 가능. */}
                 </div>
 
                 <div className="footer_column">
                     <h3>프로젝트 팀 소개</h3>
                     <ul>
-                        <li><a href="#about">about us</a></li>
-                        <li><a href="#team">project team</a></li>
-                        <li><a href="#none">its just text</a></li>
+                        <li><a>about us</a></li>
+                        <li><a>project team</a></li>
+                        <li><a>its just text</a></li>
                     </ul>
                 </div>
 
                 <div className="footer_column">
                     <h3>고객 지원</h3>
                     <ul>
-                        <li><a href="#faq">FAQ</a></li>
-                        <li><a href="#contact">contact us</a></li>
-                        <li><a href="#help">help</a></li>
+                        <li><a>FAQ</a></li>
+                        <li><a>contact us</a></li>
+                        <li><a>help</a></li>
                     </ul>
                 </div>
 
                 <div className="footer_column">
                     <h3>SNS</h3>
                     <ul>
-                        <li><a href="#twitter">Twitter</a></li>
-                        <li><a href="#facebook">Facebook</a></li>
-                        <li><a href="#instagram">Instagram</a></li>
+                        <li><a>Twitter</a></li>
+                        <li><a>Facebook</a></li>
+                        <li><a>Instagram</a></li>
                     </ul>
                 </div>
             </div>
