218d3fa3bd436936fd11882ab6bab14cf735f704
TAEJOON <hazukinoharuka@gmail.com>
2025-06-23 17:29:42 +0900

    0

--- Gonna_be_OK/src/components/Banner.jsx
+++ Gonna_be_OK/src/components/Banner.jsx
@@ -4,7 +4,7 @@ import '../CSS/Banner_style.css';
 function Banner() {
     return (
         <div className='main_banner'>
-            <img src="../img/Banner.png" alt="banner" />
+            <img src="./src/img/Banner.png" alt="banner" />
         </div>
     )
 
--- Gonna_be_OK/src/components/Header.jsx
+++ Gonna_be_OK/src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
         <header className='main_header'>
             <div className='header_wrap'>
                 <div className='header_logo'>
-                    <img src=".\src\img\Vitejs_logo.png" alt="image" />
+                    <img src='./src/img/Vitejs_logo.png' alt="image" />
                     <span>상담게시판</span>
                 </div>
                 <nav className='header_nav'>
@@ -19,11 +19,11 @@ function Header() {
                 </nav>
 
                 <div className='header_user'>
-                    <button className='header_login'>
+                    <button  className='header_login'>
                         로그인
                     </button>
                     <button className='header_signup'>
-                        회원가입
+                        <a href="signup">회원가입</a>
                     </button>
                 </div>
             </div>
