1ff2fcc986cba4ae902f4d7d9dc5785b416ff9bd
Temmie-Tem <78102157+Temmie-Tem@users.noreply.github.com>
2025-06-24 14:51:08 +0900

    Revert "Implement signup API"

--- Gonna_be_OK_Backend/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id VARCHAR(20) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL,
-    name VARCHAR(50),
-    email VARCHAR(255),
-    birth_date DATE
-);
